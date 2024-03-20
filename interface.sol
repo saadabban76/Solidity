2d521bee7e2eae93a84a48278baf9419c46f6af2
Just Saad <115649011+saadabban76@users.noreply.github.com>
2024-03-20 16:16:33 +0530

    Create interface.sol

---
+++ interface.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+// Interface : interfaces acts like a blueprint for communition between contracts by specifying function signatures(dataTypes) without implementions(declaration).
+
+contract Counter {
+    uint256 public count;
+
+    function increment() external {
+        count += 1;
+    }
+
+    function test() public pure returns (string memory) {
+        return "working ? ";
+    }
+}
+
+interface ICounter {
+    // functions inside the interface can only have external visibility for the function.
+    function count() external view returns (uint256);
+
+    function increment() external;
+}
+
+contract MyContract {
+    function incrementCounter(address _counterAdrr) external {
+        ICounter(_counterAdrr).increment();
+    }
+
+    function getCount(address _counterAdrr) external view returns (uint256) {
+        return ICounter(_counterAdrr).count();
+    }
+}
