806883e6f5d36b134075a2d93309a58ff45cd257
Just Saad <115649011+saadabban76@users.noreply.github.com>
2024-03-21 20:24:04 +0530

    Create ContractCreationFromContract.sol

---
+++ ContractCreationFromContract.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+contract Car {
+    address public owner;
+    string public model;
+    address public carAdrr;
+
+    constructor(address _owner, string memory _model) payable {
+        owner = _owner;
+        model = _model;
+    }
+}
+
+contract CarFactory {
+    Car[] public cars;
+
+    function create(string memory _model) public {
+        // code to create the new contract inside the contract.
+        Car newCar = new Car(address(this), _model);
+        cars.push(newCar);
+    }
+
+    function createAndSendEther(address _owner, string memory _model) public payable {
+        // code to create the new contract inside the contract.
+        Car newCar = (new Car){value: msg.value}(_owner, _model);
+    }
+
+    function listCars() view public returns(Car[] memory) {
+        return cars;
+    }
+}
